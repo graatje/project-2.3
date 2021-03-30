77e2f96ce300acfb30fa08bbf36758c5a2f959f1
2zqa <25235249+2zqa@users.noreply.github.com>
2021-03-30 14:02:23 +0200

    Attempt at GUI
    Co-Authored-By: Naomi Verkade <57986773+pluficorn@users.noreply.github.com>

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,24 +1,23 @@
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+    public void start(Stage primaryStage) throws Exception {
+        // Get root
+        Parent root = FXMLLoader.load(getClass().getResource("ui.fxml"));
 
-    public static void main(String[] args) {
-        launch();
-    }
+        // Misc
+        primaryStage.setTitle("Bordspellen");
 
+        // Run
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
 }
