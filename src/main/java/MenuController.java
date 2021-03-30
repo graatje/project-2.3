5769da1f138f75ee2e49bdd3edc870f2deeef7ab
2zqa <25235249+2zqa@users.noreply.github.com>
2021-03-30 17:25:54 +0200

    Add TicTacToe button functionality

--- src/main/java/MenuController.java
+++ src/main/java/MenuController.java
@@ -1,12 +1,23 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MenuController {
     @FXML private Text actiontarget;
 
-    @FXML protected void pressTTTMenuButton(ActionEvent event) {
-        actiontarget.setText("BKE pressed");
+    @FXML
+    public void pressTTTMenuButton(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("TTTMenu.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 
     @FXML protected void pressOthelloMenuButton(ActionEvent event) {
