b11b17231187fa107d696928feded62e6fe220b5
2zqa <25235249+2zqa@users.noreply.github.com>
2021-03-31 12:55:59 +0200

    WIP Draw lines in pane
    Doesn't work

--- src/main/java/TTTGameController.java
+++ src/main/java/TTTGameController.java
@@ -1,12 +1,33 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
 
-public class TTTGameController{
-    @FXML private Text actionGame;
+import java.util.ArrayList;
 
-    @FXML public void pressButton(ActionEvent event) {
-        actionGame.setText("play game");
+public class TTTGameController{
+    @FXML
+    private Pane TTTBoard;
+
+    public TTTGameController() {
+        int gridSize = 4;
+        int boardWidth = 640; // later: grootte dynamisch opvragen?
+        int boardHeight = 480;
+        float cellWidth = boardWidth/(gridSize+1);
+
+        // 4 gridboxes => 5 lines |1|2|3|4|
+        for(int i=0;i<gridSize+1;i++) {
+            float x = cellWidth*i;
+
+            Line horizontalDivider = new Line();
+            horizontalDivider.setStartX(x);
+            horizontalDivider.setStartY(0.0f);
+            horizontalDivider.setEndX(x);
+            horizontalDivider.setEndY(100.0f);
+
+            TTTBoard.getChildren().add(horizontalDivider);
         }
+    }
 }
