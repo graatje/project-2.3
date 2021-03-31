7c52cff015a8cf244d007d6a6ad8f6520d4ac16d
Naomi Verkade <57986773+pluficorn@users.noreply.github.com>
2021-03-31 15:10:04 +0200

    Little changes
    Deleted some unneeded import statements + methods

--- src/main/java/TTTMenuController.java
+++ src/main/java/TTTMenuController.java
@@ -4,16 +4,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.text.Text;
-import javafx.stage.Popup;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class TTTMenuController {
-    @FXML private Text doSomething;
-
     @FXML public void pressPlayAgainstComputer(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("TTTGame.fxml"));
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -21,11 +15,10 @@ public class TTTMenuController {
         stage.show();
     }
 
-    @FXML public void pressPlayOnline(ActionEvent event) {
-        doSomething.setText("Speel online");
-    }
-
     @FXML public void pressOKUsername(ActionEvent event) {
 
+    }
+
+    public void pressPlayOnline(ActionEvent event) {
     }
 }
