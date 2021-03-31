018adb4256970b869d6b9fa210fa6e90543d5e7c
2zqa <25235249+2zqa@users.noreply.github.com>
2021-03-31 13:21:17 +0200

    Update TTTGameController.java

--- src/main/java/TTTGameController.java
+++ src/main/java/TTTGameController.java
@@ -16,22 +16,28 @@ public class TTTGameController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        int gridSize = 4;
-        int boardWidth = 640; // later: grootte dynamisch opvragen?
-        int boardHeight = 480;
-        float cellWidth = boardWidth/(gridSize+1);
+        int gridSize = 3;
+        double boardSize = TTTBoard.getPrefWidth();
+        System.out.println("board size: "+boardSize);
 
-        // 4 gridboxes => 5 lines |1|2|3|4|
-        for(int i=0;i<gridSize+1;i++) {
-            float x = cellWidth*i;
+        // 3 gridboxes => 2 lines 1|2|3
+        for(int i=0;i<gridSize-1;i++) {
+            double pos = (double) (i+1)/gridSize*boardSize;
 
             Line horizontalDivider = new Line();
-            horizontalDivider.setStartX(x);
+            horizontalDivider.setStartX(pos);
             horizontalDivider.setStartY(0.0f);
-            horizontalDivider.setEndX(x);
-            horizontalDivider.setEndY(100.0f);
+            horizontalDivider.setEndX(pos);
+            horizontalDivider.setEndY(boardSize);
+
+            Line verticalDivider = new Line();
+            verticalDivider.setStartX(0.0f);
+            verticalDivider.setStartY(pos);
+            verticalDivider.setEndX(boardSize);
+            verticalDivider.setEndY(pos);
 
             TTTBoard.getChildren().add(horizontalDivider);
+            TTTBoard.getChildren().add(verticalDivider);
         }
     }
 }
