8f2665214eaedc624d5e54e12d0159f0840e4514
Naomi Verkade <57986773+pluficorn@users.noreply.github.com>
2021-04-06 16:29:04 +0200

    Game information added to GUI

--- src/main/java/gui/model/GenericGameModel.java
+++ src/main/java/gui/model/GenericGameModel.java
@@ -6,6 +6,7 @@ import framework.board.BoardObserver;
 import framework.board.BoardPiece;
 import framework.player.LocalPlayer;
 import framework.player.Player;
+import gui.view.GameView;
 import gui.view.View;
 import javafx.application.Platform;
 
@@ -14,6 +15,7 @@ public class GenericGameModel extends Model implements BoardObserver {
     private final Board board;
     private GameManager gameManager;
     private double boardSize = 500; //TODO: opvragen van fxml?
+    private String infoMessage;
 
     public GenericGameModel(GameManager gameManager) {
         this.gameManager = gameManager;
@@ -32,10 +34,13 @@ public class GenericGameModel extends Model implements BoardObserver {
             if(gameManager.getBoard().isValidMove(xTile, yTile)) {
                 ((LocalPlayer) player).executeMove(xTile, yTile);
             } else {
-                System.out.println("Ongeldige zet");
+                infoMessage = "Invalid move";
             }
-        } else {
-            System.out.println("Wacht op je beurt");
+            } else {
+            infoMessage = "Wait for your turn";
+            }
+        for(View view : observers) {
+            ((GameView)view).setInfoText(infoMessage);
         }
     }
 
