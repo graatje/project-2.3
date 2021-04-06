e38f107ed56c60b79ecce5c667e83f7be29b551b
Ew Its Mike <m.land@live.nl>
2021-04-06 14:54:09 +0200

    Added ConfigData class for use in GUI

---
+++ src/main/java/framework/ConfigData.java
@@ -0,0 +1,67 @@
+package framework;
+
+public class ConfigData {
+    private static ConfigData instance;
+
+    private String serverIP;
+    private int serverPort;
+
+    private String playerName;
+    private String gameType;
+
+
+    public static ConfigData getInstance() {
+        if (instance == null){
+            instance = new ConfigData();
+        }
+
+        return instance;
+    }
+
+    private ConfigData(){
+        serverIP = "main-vps.woutergritter.me";
+        serverPort = 7789;
+        playerName = "Group C4";
+    }
+
+
+    public String getServerIP() {
+        return serverIP;
+    }
+
+    public void setServerIP(String serverIP) {
+        this.serverIP = serverIP;
+    }
+
+    public int getServerPort() {
+        return serverPort;
+    }
+
+    public void setServerPort(int serverPort) {
+        this.serverPort = serverPort;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public String getGameType() {
+        return gameType;
+    }
+
+    public void setGameType(String gameType) {
+        switch (gameType){
+            case "Tic-tac-toe":
+            case "Othello":
+                this.gameType = gameType;
+                break;
+            default:
+                throw new RuntimeException("Invalid gametype! Please use \"Tic-tac-toe\" or \"Othello\"");
+        }
+
+    }
+}
