67a6403cba13723d7e8646eb27d0edfe886120fd
Wouter Gritter <woutergritter@gmail.com>
2021-04-06 17:09:08 +0200

    Use the correct start pattern

--- src/main/java/othello/board/OthelloBoard.java
+++ src/main/java/othello/board/OthelloBoard.java
@@ -216,9 +216,9 @@ public class OthelloBoard extends Board {
 
 	@Override
 	public void prepareBoard(Player startPlayer) {
-		getBoardPiece(3,3).setOwner(startPlayer);
-		getBoardPiece(4,4).setOwner(startPlayer);
-		getBoardPiece(3,4).setOwner(gameManager.getOtherPlayer(startPlayer));
-		getBoardPiece(4,3).setOwner(gameManager.getOtherPlayer(startPlayer));
+		getBoardPiece(3,3).setOwner(gameManager.getOtherPlayer(startPlayer));
+		getBoardPiece(4,4).setOwner(gameManager.getOtherPlayer(startPlayer));
+		getBoardPiece(3,4).setOwner(startPlayer);
+		getBoardPiece(4,3).setOwner(startPlayer);
 	}
 }
