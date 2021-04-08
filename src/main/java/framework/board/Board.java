7f2eacb4f974150c73d8d8d407cc30c5385b3da8
Ew Its Mike <m.land@live.nl>
2021-04-08 13:41:21 +0200

    Fixed the "Horrible mess" TODO's, and all that it entailed

--- src/main/java/framework/board/Board.java
+++ src/main/java/framework/board/Board.java
@@ -6,6 +6,7 @@ import framework.ConnectedGameManager;
 import framework.GameManager;
 import framework.player.MoveRequestable;
 import framework.player.Player;
+import framework.player.ServerPlayer;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -97,8 +98,7 @@ public abstract class Board implements Cloneable {
     /**
      * this method requests a playermove from the board if all players have been initialized.
      */
-    // TODO: FIX THIS HORRIBLE MESS!!!!!!
-    public void start(Player startingPlayer, Player actualStartingPlayer, boolean requestFirstPlayerMove) {
+    public void start(Player startingPlayer) {
         if (boardState != BoardState.WAITING) {
             throw new IllegalStateException("The game cannot start in this state! (Current state: " + boardState + ")");
         }
@@ -116,34 +116,16 @@ public abstract class Board implements Cloneable {
             startingPlayer = gameManager.getPlayer((int) (Math.random() * gameManager.getNumPlayers()));
         }
 
-        // TODO: FIX THIS HORRIBLE MESS TOO!!
-        if (actualStartingPlayer == null) {
-            actualStartingPlayer = startingPlayer;
-        }
-
         currentPlayerId = startingPlayer.getID();
 
-        prepareBoard(actualStartingPlayer);
+
+        prepareBoard(startingPlayer);
 
         boardState = BoardState.PLAYING;
 
         for (BoardObserver o : observers) {
-            // TODO: FIX THIS HORRIBLE MESS
-            o.onGameStart(actualStartingPlayer);
+            o.onGameStart(startingPlayer);
         }
-
-        // Request a move from the first player
-        if (requestFirstPlayerMove) {
-            requestPlayerMove();
-        }
-    }
-
-    public void start(Player startingPlayer, boolean requestFirstPlayerMove) {
-        start(startingPlayer, startingPlayer, requestFirstPlayerMove);
-    }
-
-    public void start(Player startingPlayer) {
-        start(startingPlayer, true);
     }
 
     public void reset() {
@@ -275,7 +257,7 @@ public abstract class Board implements Cloneable {
      */
     public void forceWin(Player winner) {
 
-        if (this.boardState != BoardState.PLAYING){
+        if (this.boardState != BoardState.PLAYING) {
             return;
         }
 
@@ -361,7 +343,8 @@ public abstract class Board implements Cloneable {
      * @return The player who is currently expected to make a move.
      */
     public Player getCurrentPlayer() {
-        if(currentPlayerId < 0 || currentPlayerId >= gameManager.getNumPlayers()) {
+        if (currentPlayerId < 0 || currentPlayerId >= gameManager.getNumPlayers()) {
+            System.err.println("Returning NULL for getCurrentPlayer! ID was: " + currentPlayerId);
             return null;
         }
 
