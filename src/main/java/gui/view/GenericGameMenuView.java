1dd3f1c2136a397d6c034069e669a7f99b6db184
2zqa <25235249+2zqa@users.noreply.github.com>
2021-04-08 12:29:47 +0200

    Actually use generics in Views

--- src/main/java/gui/view/GenericGameMenuView.java
+++ src/main/java/gui/view/GenericGameMenuView.java
@@ -1,18 +1,19 @@
 package gui.view;
 
 import gui.controller.Controller;
+import gui.model.GenericGameModel;
 import gui.model.Model;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.text.Text;
 
-public class GenericGameMenuView extends View {
+public class GenericGameMenuView extends View<GenericGameModel> {
     public GenericGameMenuView(Parent parent, Controller controller, int windowWidth, int windowHeight) {
         super(parent, controller, windowWidth, windowHeight);
     }
 
     @Override
-    public void update(Model model) {
+    public void update(GenericGameModel model) {
 
     }
 }
